d4cda86412eab37e05c4d1b9d2ad10233e8af9cc
Rhys Moyne <rhysmoyne@users.noreply.github.com>
2016-09-10 02:39:50 +0930

    Create travis-install.sh

---
+++ travis-install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+git submodule init && \
+git config user.name "Rhys Moyne" && \
+git config user.email "rhys@creativekidssa.com.au" && \
+git submodule update && \
+git submodule update --remote data/pages && \
+git add data/pages && \
+git commit -m "Update wiki to latest content."; \
+git push -u origin master && \
+git status
