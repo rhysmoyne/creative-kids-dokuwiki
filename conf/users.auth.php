e31d2500166ab8a1d8d5c6b676f49316ec9367dd
Rhys Moyne <rhys@creativekidssa.com.au>
2016-09-09 23:02:03 +0930

    Initialise with install.php

---
+++ conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+admin:$1$bsfMUI13$II6NWx3.QnRZDsXaFVTmz.:Creative Kids Admin:admin@creativekidssa.com.au:admin,user
